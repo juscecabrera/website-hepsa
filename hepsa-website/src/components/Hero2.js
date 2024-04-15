eee4fd33c156be07d28cc3a2cdd8493a86e70bdb
Jusce Cabrera <jusce240@gmail.com>
2024-04-15 14:11:57 -0500

    creo hero2

---
+++ hepsa-website/src/components/Hero2.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../stylesheets/main.css";
+import Hero2Img from "../images/imgHepsaIndex4.jpg"
+
+function Hero2() {
+    return (
+        <>
+        <img src={Hero2Img}></img>
+        <h1>Hepsa</h1>
+        <h2>
+Somos una empresa con más de 60 años en el sector de venta industrial de Carburo de Calcio.</h2>
+        </>
+    )
+}
+
+export default Hero2;
