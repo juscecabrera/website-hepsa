2a3b5fbc82744afd9ae85e8c4388c665f206fa02
Jusce Cabrera <jusce240@gmail.com>
2024-04-19 10:35:05 -0500

    agrego responsive a hero2

--- hepsa-website/src/components/About.js
+++ hepsa-website/src/components/About.js
@@ -15,7 +15,7 @@ function About () {
         <>
         <div className="about-wrapper" id="aboutid">
             <h2 className="about-h2">NOSOTROS</h2>
-            <p className="about-p"> Somos una empresa con más de 60 años en el sector de la venta industrial de Carburo de Calcio. Somos la única empresa peruana de producción y venta de Carburo de Calcio.  
+            <p className="about-p"> Somos una empresa con más de 60 años en el sector de la venta industrial de Carburo de Calcio. Somos la única empresa peruana de producción y venta de Carburo de Calcio. Actualmente proveemos a más de 10 compañías dentro del sector industrial.
             </p>
             <div className="about-img-wrapper"><img src={ContentImg4}/><img src={ContentImg5}/><img src={ContentImg7}/></div>
         </div>
